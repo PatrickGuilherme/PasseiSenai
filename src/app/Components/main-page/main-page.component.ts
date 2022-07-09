c09c2c8f86d574fc45c002c7f7519ce7dc124833
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2022-07-08 22:29:23 -0300

    Finalização Parte 2

--- src/app/Components/main-page/main-page.component.ts
+++ src/app/Components/main-page/main-page.component.ts
@@ -11,22 +11,41 @@ import { ApiService } from 'src/app/Service/api.service';
 export class MainPageComponent implements OnInit {
   private Media:Result;
   public gradeForm:FormGroup = this.formbuilder.group({
-    av1: [null, [Validators.required, Validators.min(0), Validators.max(10)]],
-    av2: [null, [Validators.required, Validators.min(0), Validators.max(10)]],
-    av3: [null, [Validators.required, Validators.min(0), Validators.max(10)]],
-    edag: [null, [Validators.required, Validators.min(0), Validators.max(10)]]
+    av1: [undefined, [Validators.required, Validators.min(0), Validators.max(10)]],
+    av2: [undefined, [Validators.required, Validators.min(0), Validators.max(10)]],
+    av3: [undefined, [Validators.required, Validators.min(0), Validators.max(10)]],
+    edag: [undefined, [Validators.required, Validators.min(0), Validators.max(10)]]
   });
 
   ngOnInit(): void {
   }
 
   constructor(private formbuilder:FormBuilder, 
-            private apiservice:ApiService) {
+    private apiservice:ApiService) {
+  }
+
+  public GetGrade(gradeTipo:string){
+    let valueResturn:number;
+    switch(gradeTipo){
+      case "av1":
+        valueResturn =  this.Arred(this.gradeForm.value.av1);
+      break;
+      case "av2":
+        valueResturn =  this.Arred(this.gradeForm.value.av2);
+      break;
+      case "av3":
+        valueResturn =  this.Arred(this.gradeForm.value.av3);
+      break;
+      case "edag":
+        valueResturn =  this.Arred(this.gradeForm.value.edag);
+      break;
+    }
+    return valueResturn;
   }
 
   public VerifyErros(variable:string):string {
-    if(!variable) return "";
-
+    if(!this.gradeForm.dirty) return "";
+    else
     if(this.gradeForm.getError('required',variable)) return "Campo obrigatório";
     else 
     if(this.gradeForm.getError('min',variable)) return "Valor minimo de 0";
@@ -35,25 +54,33 @@ export class MainPageComponent implements OnInit {
     else return "";
   }
 
+  public ResultMediaFinal():number{
+    return this.Arred((50-6*this.Media.Grade)/4);
+  }
+
   public ResultMedia():Result {
     return this.Media;
   }
 
+  private Arred(media:number):number { 
+    var aux = Math.pow(10,1)
+    return Math.floor(media * aux)/aux
+ }
+
   public OnSubmit() {
-    let av1 = this.gradeForm.value.av1;
-    let av2 = this.gradeForm.value.av2;
-    let av3 = this.gradeForm.value.av3;
-    let edag = this.gradeForm.value.edag;
-    let calcResult = ((av1*25)+(av2*25)+(av3*30)+(edag*20))/100
     let colorGrade:string;
     let textMotivation:string;
+    let av1:number = this.gradeForm.value.av1;
+    let av2:number = this.gradeForm.value.av2;
+    let av3:number = this.gradeForm.value.av3;
+    let edag:number = this.gradeForm.value.edag;
+
+    let calcResult:number = ((av1*25)+(av2*25)+(av3*30)+(edag*20))/100
+    calcResult = this.Arred(calcResult);
 
     if(calcResult < 7){
       colorGrade = "#dc3545";
-    } else 
-    if(calcResult >= 7 && calcResult <= 7.9){
-      colorGrade = "#ffc107";
-    }else{
+    } else{
       colorGrade = "#28a745";
     }
 
