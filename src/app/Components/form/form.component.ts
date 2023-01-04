be0cd87c7a4bfdf06acd9f5b1e48666362a1c54b
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2023-01-04 00:41:49 -0300

    Finalização das validações do formulario

--- src/app/Components/form/form.component.ts
+++ src/app/Components/form/form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Grade } from 'src/app/Models/Grade';
 
 @Component({
   selector: 'app-form',
@@ -8,14 +9,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class FormComponent implements OnInit {
   public gradeForm:FormGroup;
+  public Grade:Grade;
+
   constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
     this.gradeForm = this.formbuilder.group({
-      av1: ['', [Validators.required, Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
-      av2: ['', [Validators.required, Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
-      av3: ['', [Validators.required, Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
-      edag:['', [Validators.required, Validators.min(0), Validators.max(10), Validators.maxLength(3)]]
+      av1: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
+      av2: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
+      av3: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
+      edag:['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]]
     });
   }
 
@@ -27,8 +30,20 @@ export class FormComponent implements OnInit {
     else
     if(this.gradeForm.getError('max',variable) && this.gradeForm.get(variable).touched) return "Valor máximo de 10";
     else 
-    if(typeof(parseFloat(this.gradeForm.get(variable).value)) != 'number' && this.gradeForm.get(variable).touched) return "Valor Inválido 2";
-    else
+    if(this.gradeForm.getError('pattern',variable) && this.gradeForm.get(variable).touched) return "Valor inválido";
     return "";
+  }
+
+  public SubmitForm():void{
+    if(this.gradeForm.valid){
+      this.Grade = new Grade(
+        parseFloat(this.gradeForm.value.av1),
+        parseFloat(this.gradeForm.value.av2),
+        parseFloat(this.gradeForm.value.av3),
+        parseFloat(this.gradeForm.value.edag)
+      );
+      console.log(this.Grade.CalcMedia());
+      console.log(this.Grade.ResultMediaFinal(this.Grade.CalcMedia()));
+    }
   }
 }
