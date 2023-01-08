4317e854456480d49ef8129d947c3fdec07ee225
Patrick Guilherme da Silva <patrick.guilhermedasilva@gmail.com>
2023-01-07 23:18:01 -0300

    Capturar erros dos inputs

--- src/app/Components/form/form.component.ts
+++ src/app/Components/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
-import { Grade } from 'src/app/Models/Grade';
+import { Estudante } from 'src/app/Models/Estudante';
 
 @Component({
   selector: 'app-form',
@@ -8,13 +8,13 @@ import { Grade } from 'src/app/Models/Grade';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  public gradeForm:FormGroup;
-  public Grade:Grade;
+  public estudanteForm:FormGroup;
+  public estudante:Estudante;
 
   constructor(private formbuilder:FormBuilder) { }
 
   ngOnInit(): void {
-    this.gradeForm = this.formbuilder.group({
+    this.estudanteForm = this.formbuilder.group({
       av1: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
       av2: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
       av3: ['', [Validators.required, Validators.pattern('[0-9]+([,\.][0-9]+)?'), Validators.min(0), Validators.max(10), Validators.maxLength(3)]],
@@ -24,26 +24,24 @@ export class FormComponent implements OnInit {
 
   //Verifcação de erros 
   public VerifyErros(variable:string):string {
-    if(this.gradeForm.getError('required',variable) && this.gradeForm.get(variable).touched) return "Campo obrigatório";
+    if(this.estudanteForm.getError('required',variable) && this.estudanteForm.get(variable).touched) return "Campo obrigatório";
     else 
-    if(this.gradeForm.getError('min',variable) && this.gradeForm.get(variable).touched) return "Valor mínimo de 0";
+    if(this.estudanteForm.getError('min',variable) && this.estudanteForm.get(variable).touched) return "Valor mínimo de 0";
     else
-    if(this.gradeForm.getError('max',variable) && this.gradeForm.get(variable).touched) return "Valor máximo de 10";
+    if(this.estudanteForm.getError('max',variable) && this.estudanteForm.get(variable).touched) return "Valor máximo de 10";
     else 
-    if(this.gradeForm.getError('pattern',variable) && this.gradeForm.get(variable).touched) return "Valor inválido";
+    if(this.estudanteForm.getError('pattern',variable) && this.estudanteForm.get(variable).touched) return "Valor inválido";
     return "";
   }
 
   public SubmitForm():void{
-    if(this.gradeForm.valid){
-      this.Grade = new Grade(
-        parseFloat(this.gradeForm.value.av1),
-        parseFloat(this.gradeForm.value.av2),
-        parseFloat(this.gradeForm.value.av3),
-        parseFloat(this.gradeForm.value.edag)
+    if(this.estudanteForm.valid){
+      this.estudante = new Estudante(
+        parseFloat(this.estudanteForm.value.av1),
+        parseFloat(this.estudanteForm.value.av2),
+        parseFloat(this.estudanteForm.value.av3),
+        parseFloat(this.estudanteForm.value.edag)
       );
-      console.log(this.Grade.CalcMedia());
-      console.log(this.Grade.ResultMediaFinal(this.Grade.CalcMedia()));
     }
   }
 }
